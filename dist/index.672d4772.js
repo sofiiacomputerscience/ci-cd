d007d42fbbda8a4a1677b7dffa63ac9c0c717bf8
Sofiia Boldeskul <sofiia.computerscience@gmail.com>
2025-01-20 09:47:18 +0100

    New version package

---
+++ dist/index.672d4772.js
@@ -0,0 +1,4 @@
+// index.js
+console.log('Welcome to the Simple Website!');
+
+//# sourceMappingURL=index.672d4772.js.map
---
+++ dist/index.fc5af49b.js
@@ -0,0 +1,2 @@
+console.log("Welcome to the Simple Website!");
+//# sourceMappingURL=index.fc5af49b.js.map
